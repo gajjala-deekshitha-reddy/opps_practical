ee148fdc8155ee673ab3e6423c69f17abf063295
gajjala-deekshitha-reddy <2400031669@klh.edu.in>
2025-08-26 12:36:20 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,15 @@
+package oop;
+
+class Logical {
+	  public static void main(String[] args) {
+	    System.out.println((5 > 3) && (8 > 5));  
+	    System.out.println((5 > 3) && (8 < 5));  
+	    
+	    System.out.println((5 < 3) || (8 > 5));  
+	    System.out.println((5 > 3) || (8 < 5));  
+	    System.out.println((5 < 3) || (8 < 5));  
+
+	    System.out.println(!(5 == 3));  
+	    System.out.println(!(5 > 3));  
+	  }
+	}
